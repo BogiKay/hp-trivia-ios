62d1e57856db2cc69165f1a99048e3b3cffea262
Bogusz Kaszowski <bogusz.kaszowski@callstack.com>
2024-05-26 12:35:36 +0200

    styling of home screen

---
+++ HP Trivia/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  HP Trivia
+//
+//  Created by Bogusz Kaszowski on 26/05/2024.
+//
+
+import Foundation
+
+enum Constants {
+    static let hpFont = "PartyLetPlain"
+}
--- HP Trivia/ContentView.swift
+++ HP Trivia/ContentView.swift
@@ -9,13 +9,87 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        GeometryReader { geo in
+            ZStack {
+                Image("hogwarts")
+                    .resizable()
+                    .frame(width: geo.size.width * 3, height: geo.size.height)
+                    .padding(.top, 3)
+                VStack {
+                    
+                    VStack {
+                        Image(systemName: "bolt.fill")
+                            .font(.largeTitle)
+                            .imageScale(.large)
+                        Text("HP")
+                            .font(.custom(Constants.hpFont, size: 70))
+                            .padding(.bottom, -50)
+                        Text("Trivia")
+                            .font(.custom(Constants.hpFont, size: 60))
+                    }
+                    .padding(.top, 70)
+                    
+                    Spacer()
+                    
+                    VStack {
+                        Text("Recent scores")
+                            .font(.title2)
+                        
+                        Text("33")
+                        Text("27")
+                        Text("15")
+                    }
+                    .font(.title3)
+                    .padding(.horizontal)
+                    .foregroundColor(.white)
+                    .background(.black.opacity(0.7))
+                    .cornerRadius(15)
+
+                    
+                    Spacer()
+                    
+                    HStack {
+                        Button {
+                          // show game instructions
+                        } label: {
+                            Image(systemName: "info.circle.fill")
+                                .font(.largeTitle)
+                                .foregroundColor(.white)
+                                .shadow(radius: 5)
+                            
+                        }
+                        Spacer()
+                        Button {
+                            // plage
+                        } label: {
+                            Text("Play")
+                                .font(.largeTitle)
+                                .foregroundColor(.white)
+                                .padding(.vertical, 7)
+                                .padding(.horizontal, 50)
+                                .background(.brown)
+                                .cornerRadius(7)
+                                .shadow(radius: 5)
+                        }
+                       
+                        Spacer()
+                        Button {
+                            // settings
+                        } label: {
+                            Image(systemName: "gearshape.fill")
+                                .font(.largeTitle)
+                                .foregroundColor(.white)
+                                .shadow(radius: 5)
+                        }
+                    }
+                    .padding(.horizontal, 30)
+                    .frame(width: geo.size.width)
+                    
+                    Spacer()
+                }
+            }
+            .frame(width: geo.size.width, height: geo.size.height)
+        }.ignoresSafeArea()
     }
 }
 
