013d088285714f56043819df0d4cede9e71805c1
Bogusz Kaszowski <bogusz.kaszowski@callstack.com>
2024-05-26 12:06:30 +0200

    Initial Commit

---
+++ HP Trivia/HP_TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HP_TriviaApp.swift
+//  HP Trivia
+//
+//  Created by Bogusz Kaszowski on 26/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct HP_TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
