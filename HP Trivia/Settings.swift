a095f2d95cf3a6ffece2b97a0ea4d8121524d233
Bogusz Kaszowski <bogusz.kaszowski@callstack.com>
2024-06-04 21:34:33 +0200

    add some interactions on settings screen

--- HP Trivia/Settings.swift
+++ HP Trivia/Settings.swift
@@ -7,8 +7,15 @@
 
 import SwiftUI
 
+enum BookStatus {
+    case active
+    case inactive
+    case locked
+}
+
 struct Settings: View {
     @Environment(\.dismiss) private var dismiss;
+    @State private var books: [BookStatus] = [.active, .active, .inactive, .locked, .locked, .locked, .locked]
 
     var body: some View {
         ZStack {
@@ -17,50 +24,67 @@ struct Settings: View {
                 Text("Which books would you like to see questions from?")
                     .font(.title)
                     .multilineTextAlignment(.center)
+                    .padding(.top)
                 
                 ScrollView {
                     LazyVGrid(columns: [GridItem(), GridItem()], content: {
-                        ZStack(alignment: .bottomTrailing) {
-                            Image("hp1")
-                                .resizable()
-                                .scaledToFit()
-                            .shadow(radius: 7)
+                        ForEach(0..<7) { i in
                             
-                            Image(systemName: "checkmark.circle.fill")
-                                .font(.largeTitle)
-                                .imageScale(.large)
-                                .foregroundColor(.green)
-                                .shadow(radius: 1)
-                                .padding(3)
-                        }
-                        
-                        ZStack(alignment: .bottomTrailing) {
-                            Image("hp2")
-                                .resizable()
-                                .scaledToFit()
-                            .shadow(radius: 7)
-                            .overlay(Rectangle().opacity(0.33))
+                            if books[i] == .active {
+                                ZStack(alignment: .bottomTrailing) {
+                                    Image("hp\(i + 1)")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .shadow(radius: 7)
+                                    
+                                    Image(systemName: "checkmark.circle.fill")
+                                        .font(.largeTitle)
+                                        .imageScale(.large)
+                                        .foregroundColor(.green)
+                                        .shadow(radius: 1)
+                                        .padding(3)
+                                }
+                                .onTapGesture {
+                                    books[i] = .inactive
+                                }
+                            }
+                               
                             
-                            Image(systemName: "circle")
-                                .font(.largeTitle)
-                                .imageScale(.large)
-                                .foregroundColor(.green.opacity(0.5))
-                                .shadow(radius: 1)
-                                .padding(3)
-                        }
-                        
-                        ZStack(alignment: .center) {
-                            Image("hp3")
-                                .resizable()
-                                .scaledToFit()
-                            .shadow(radius: 7)
-                            .overlay(Rectangle().opacity(0.75))
+                            if books[i] == .inactive {
+                                ZStack(alignment: .bottomTrailing) {
+                                    Image("hp\(i + 1)")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .shadow(radius: 7)
+                                        .overlay(Rectangle().opacity(0.33))
+                                    
+                                    Image(systemName: "circle")
+                                        .font(.largeTitle)
+                                        .imageScale(.large)
+                                        .foregroundColor(.green.opacity(0.5))
+                                        .shadow(radius: 1)
+                                        .padding(3)
+                                }
+                                .onTapGesture {
+                                    books[i] = .active
+                                }
+                            }
                             
-                            Image(systemName: "lock.fill")
-                                .font(.largeTitle)
-                                .imageScale(.large)
-                                .shadow(color: .white.opacity(0.75), radius: 3)
-                                .padding(3)
+                            if books[i] == .locked {
+                                ZStack(alignment: .center) {
+                                    Image("hp\(i + 1)")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .shadow(radius: 7)
+                                        .overlay(Rectangle().opacity(0.75))
+                                    
+                                    Image(systemName: "lock.fill")
+                                        .font(.largeTitle)
+                                        .imageScale(.large)
+                                        .shadow(color: .white.opacity(0.75), radius: 3)
+                                        .padding(3)
+                                }
+                            }
                         }
                     })
                     .padding()
